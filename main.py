e461b6b585666c0762d6f12e5a8fb395dd8a491f
MechaMask <vesper.ludwig@outlook.com>
2024-10-08 01:38:28 -0400

    modularized the code

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import os
 
+def get_files_from_dir(dir_path):
+    items = os.listdir(dir_path)
+    books = {}
+    for file in items:
+        if os.path.isfile(f"{dir_path}/{file}"):
+            name,extension = os.path.splitext(file)
+            if name not in books:
+                books[name] = {"ext":extension}
+            print(f"  - {name[0].upper()}{name[1:].lower()}")
+    return books
+
+def read_book(path):
+    with open(path) as b:
+        return b.read()
+
 def main():
     print("Welcome to Book Bot!!")
     print("======================")
     print("")
 
     print("Here are a list of books you can choose:")
-    books_list = os.listdir("./books")
-    books = {}
-    for file in books_list:
-        if os.path.isfile(f"./books/{file}"):
-            name,extension = os.path.splitext(file)
-            if name not in books:
-                books[name] = {"ext":extension}
-            print(f"{name[0].upper()}{name[1:].lower()}")
-    book_name = input("Enter title of book:  ").lower()
+    books = get_files_from_dir("./books")
     contents = ""
-    file_extension = books[book_name]["ext"]
-    if book_name in books:
-        with open(f"books/{book_name}{file_extension}") as b:
-            contents = b.read()
-        print(contents)
-    else:
-        print("Title not found!")
+    while contents == "":
+        book_title = input("\nPlease enter the title of a book:  ").lower()
+        if book_title in books:
+            file_extension = books[book_title]["ext"]
+            content = read_book(f"./books/{book_title}{file_extension}")
+            break
+        else:
+            print("Title not found!")
+    print(content)
 main()
